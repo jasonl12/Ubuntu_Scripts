2b483887a1032b7822ec618de61ae8ba94f421f4
Jason L. <jason.liang331@gmail.com>
2025-06-11 09:17:27 -0700

    Update check_conn_ssl.sql

--- SQL/Postgres/check_conn_ssl.sql
+++ SQL/Postgres/check_conn_ssl.sql
@@ -1,5 +1,6 @@
 -- sudo -u postgres psql -c 'SELECT * FROM pg_stat_ssl;'
 -- sudo -u postgres psql < check_conn_ssl.sql
+-- psql -U postgres -h localhost < check_conn_ssl.sql
 
 SELECT pg_ssl.pid, pg_ssl.ssl, pg_ssl.version,
 pg_sa.backend_type, pg_sa.usename, pg_sa.client_addr
